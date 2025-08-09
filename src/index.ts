b6fbf44082756466ec7fcd6ae284e9fac1335fca
Klammertime <audrey.ck@gmail.com>
2025-08-09 02:21:43 -0700

    chore: bump version to 0.2.5
    - v0.2.4 tag already exists on remote
    - Updating to v0.2.5 to avoid conflict

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /**
  * CopilotEdge - Production-ready adapter for CopilotKit + Cloudflare Workers AI
  * @author Audrey Klammer (@Klammertime)
- * @version 0.2.4
+ * @version 0.2.5
  * @license MIT
  * 
  * Features:
